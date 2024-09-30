7a638b1412d2b4a61a22f7439d531a035c972592
Stonepaiste <mathiasjaque@hotmail.com>
2024-09-30 16:19:12 +0200

    blah blah

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,27 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Input = UnityEngine.Windows.Input;
 
 public class Player : MonoBehaviour
 {
     private Vector2 _inputs;
     public int movespeed = 50;
-    public float gravity = -9.81f;
-    private Vector3 velocity;
+    public float JumpVelocity = 5f;
+    public bool _isjumping;
+   // private Vector3 velocity;
+    //public float jumpHeight = 2.4f;
 
     public float mouseSensitivity = 100f;
     private float xRotation = 0f;
+    private Rigidbody _rb;
     
 
     private void Move()
     {
-        Vector3 move = new Vector3(_inputs.x, 0, _inputs.y);
+        Vector3 move = new Vector3(_inputs.x, _inputs.y);
         GetComponent<CharacterController>().Move(transform.TransformDirection(move) * Time.deltaTime * movespeed);
 
         // Apply gravity
-        velocity.y += gravity * Time.deltaTime;
-        GetComponent<CharacterController>().Move(velocity * Time.deltaTime);
+      //  velocity.y += gravity * Time.deltaTime;
+       // GetComponent<CharacterController>().Move(velocity * Time.deltaTime);
     }
 
     public void Movement(InputAction.CallbackContext context)
@@ -35,20 +37,37 @@ public class Player : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-       
+       // Move();
+        _isjumping |= Input.GetKeyDown(KeyCode.Space);
+        {
+            
+        }
+
     }
 
     private void FixedUpdate()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame) 
+       // if (Keyboard.current.spaceKey.wasPressedThisFrame) 
+        //{
+         //   velocity.y = Mathf.Sqrt(2f  * 1.5f);
+        //}
+        if (_isjumping)
         {
-            velocity.y = Mathf.Sqrt(2f * 9.81f * 1.5f);
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+            
         }
+
+       
+        _isjumping = false;
+    
     }
+    
+    
+   
 }
