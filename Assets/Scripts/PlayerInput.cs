b77bb9c6d1760b455ada78e88ae59b59e2cf549e
PhillzR <rumpelthiin1997@gmail.com>
2024-10-14 15:33:26 +0200

    Not completely working yet

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -6,38 +6,68 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerInput : MonoBehaviour
 {
+    Transform player;
+    Transform camera; 
     Vector2 movement;
+    float movementX;
+    float movementY;
     Rigidbody rb;
-    [SerializeField] float movespeed = 500f;
-    [SerializeField] float mouseSensitivity = 100f;
+    [SerializeField] float walkSpeed = 100f;
+    [SerializeField] float runSpeed = 200f;
+    [SerializeField] float sensitivityX = 100f;
+    [SerializeField] float sensitivityY = 100f;
+    [SerializeField] float lookX;
+    [SerializeField] float lookY;
+    bool isRunning = false; 
 
 
     void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
         rb = GetComponent<Rigidbody>();
+        player = this.transform;
+        camera = Camera.main.transform;
+
     }
 
     private void FixedUpdate()
     {
-        
+        Move();
+        //Look();
     }
+
+
 
     public void OnMove(InputValue value)
     {
         movement = value.Get<Vector2>();
-        float hAxis = movement.y;
-        float vAxis = movement.x;
+        movementY = movement.y;
+        movementX = movement.x;
+    }
 
-        Vector3 hMove = hAxis * transform.forward;
-        Vector3 vMove = vAxis * transform.right;
-        rb.velocity = (hMove + vMove).normalized * movespeed * Time.deltaTime;
-
-        Debug.Log("Moving");
+    public void OnLook(InputValue value)
+    {
+        lookX += value.Get<Vector2>().x * sensitivityX * player.rotation.y;
+        lookY -= value.Get<Vector2>().y * sensitivityY;
+        Debug.Log("looking");
     }
 
     public void OnInteract(InputValue value)
     {
 
+    }
+
+    private void Move()
+    {
+        Vector3 move = new Vector3(movementX, 0f, movementY);
+        rb.velocity = move * walkSpeed * Time.deltaTime;
+    }
+
+    private void Look()
+    {
+        Debug.Log("Looking around");
+        Vector3 lookXAxis = new Vector3(player.rotation.y + lookX, 0f, 0f);
+        Vector3 lookYAxis = new Vector3(0f, camera.rotation.x + lookY, 0f);
+        //camera.transform.rotation = Quaternion.Euler(camera.transform.rotation.x + lookY, 0, 0);
     }
 }
