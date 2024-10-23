e403284811fc2d26919f14f69575f19fcb6ac9b4
ErikaNicolaisen <147511548+ErikaNicolaisen@users.noreply.github.com>
2024-10-23 13:43:41 +0200

    New Enemy Movement

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     bool isActive;
-    [SerializeField] float moveSpeed = 50f; 
-    Vector3 playerPosition;
-    Vector3 enemyPosition;
-    Transform enemy; 
+    [SerializeField] float moveSpeed = 50f;
+    Transform player;
+    Transform enemy;
 
     void Awake()
     {
-        isActive = gameObject.activeSelf;
-        playerPosition = FindObjectOfType<PlayerInput>().transform.position;
+        player = FindObjectOfType<PlayerInput>().transform;
         enemy = this.transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (isActive)
@@ -25,10 +22,16 @@ public class EnemyMovement : MonoBehaviour
             MoveTowardsPlayer();
         }
     }
+
     public void MoveTowardsPlayer()
     {
+        Vector3 playerPosition = player.position;
         enemy.LookAt(playerPosition);
         enemy.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
 
+    public void ActivateEnemy()
+    {
+        isActive = true;
     }
 }
