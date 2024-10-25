33a42743dabaf5dd4be2c6730d10fda308fd6a9f
Mathias2023MED <145336123+Mathias2023MED@users.noreply.github.com>
2024-10-25 13:51:57 +0200

    LighthouseLamp

---
+++ Assets/Scripts/Player/PowerOff.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerOff : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PowerTrigger")
+        {
+           GetComponent<LampLighthouse>().TurnPowerOff(); //Calling method from LampLighthouse.cs
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
