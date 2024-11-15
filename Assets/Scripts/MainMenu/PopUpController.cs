9f2bf08f887533ed1da8ebf5da3904df37675a74
Sofiethansen <hansensofie@yahoo.dk>
2024-11-15 10:18:07 +0100

    Fixet main menu igen

--- Assets/Scripts/MainMenu/PopUpController.cs
+++ Assets/Scripts/MainMenu/PopUpController.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 public class PopUpController : MonoBehaviour
 {
     public GameObject popUpPanel;// Assign the pop-up Panel in the Inspector
+    public GameObject mainMenu;
     
 
     public void TogglePopUp()
     {
-        popUpPanel.SetActive(!popUpPanel.activeSelf);
+        bool isActive = !popUpPanel.activeSelf;
+        popUpPanel.SetActive(isActive);
+
+        mainMenu.SetActive(!isActive);
        
+    }
+
+    public void GoBackToMainMenu()
+    {
+        popUpPanel.SetActive(false); //Hide the pop-up panel
+        mainMenu.SetActive(true); // Show the main menu
     }
 }
 
