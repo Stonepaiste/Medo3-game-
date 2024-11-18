63e08c3a6615e93469aacdce4a7345f4f418245d
natconrad <ncol23@student.aau.dk>
2024-11-18 11:51:31 +0100

    Lidt rettelse

--- Assets/Scripts/TimelineScripts/ClickLightSwitch.cs
+++ Assets/Scripts/TimelineScripts/ClickLightSwitch.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ClickLightSwitch : MonoBehaviour
 {
     [SerializeField] private GameObject lightSwitch;
     private bool lightInRoomOn = false;
 
+    [SerializeField] private Text pressE;
+
     [SerializeField] private UnityEvent ClickSwitch;
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            pressE.enabled = true;
+        }
+    }
     void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E) && !lightInRoomOn)
         {
             lightSwitch.SetActive(true);
+
+  
+
             lightInRoomOn = true;
 
             ClickSwitch.Invoke();
